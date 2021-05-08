230fd9d6270d663696f82bcf12ba69932d9f8d89
jhondev-code <jhondev.code@gmail.com>
2021-05-08 17:43:49 -0500

    If statement

---
+++ 4.flow_control/if/index.js
@@ -0,0 +1,15 @@
+/*
+ * ===========================================================================
+ * Name        : 4.flow_control/if
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : If statement
+ * Created on  : 2021-05-08 17:23:51.447316
+ * ===========================================================================
+ */
+
+var number = 25;
+
+if (number % 2) {
+    console.log("The number is even");
+}
