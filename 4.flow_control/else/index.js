90d84651a98a824c9767cf83239071aed1ceb643
jhondev-code <jhondev.code@gmail.com>
2021-05-08 17:44:02 -0500

    Else statement

---
+++ 4.flow_control/else/index.js
@@ -0,0 +1,17 @@
+/*
+ * ===========================================================================
+ * Name        : 4.flow_control/else
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : else statement
+ * Created on  : 2021-05-08 17:24:36.446939
+ * ===========================================================================
+ */
+
+var number = 25;
+
+if (number % 2) {
+    console.log("The number is even");
+} else {
+    console.log("The number is odd")
+}
