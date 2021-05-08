6282a3e982600a66ed00440b5f656099d8758088
jhondev-code <jhondev.code@gmail.com>
2021-05-08 17:59:18 -0500

    While loop in javascript

---
+++ 5.loops/while/index.js
@@ -0,0 +1,18 @@
+/*
+ * ===========================================================================
+ * Name        : 5.loops/while
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : while loop in javascript
+ * Created on  : 2021-05-08 17:51:49.922578
+ * ===========================================================================
+ */
+
+let counter = 0;
+
+while (counter <= 20) {
+    if (counter % 2 == 0) {
+        console.log(counter);
+    }
+    counter++;
+}
