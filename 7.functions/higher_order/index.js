8238fbc224e01c59a49fcd2bc2ebf6cab79d48d0
jhondev-code <jhondev.code@gmail.com>
2021-05-08 19:33:13 -0500

    Higher-Order functions

---
+++ 7.functions/higher_order/index.js
@@ -0,0 +1,15 @@
+/*
+ * ===========================================================================
+ * Name        : 7.functions/higher_order
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : Higher-Order functions
+ * Created on  : 2021-05-08 19:29:57.649637
+ * ===========================================================================
+ */
+
+let a = function() {
+    console.log("2000 milliseconds have passed since this demo started");
+};
+
+setTimeout(a, 2000);
