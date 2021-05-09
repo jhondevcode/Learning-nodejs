e0316b16c9aa52438fdd827ab5bc262b0b31c71e
jhondev-code <jhondev.code@gmail.com>
2021-05-08 19:22:42 -0500

    Function with immediate execution

---
+++ 7.functions/immediately/index.js
@@ -0,0 +1,13 @@
+/*
+ * ===========================================================================
+ * Name        : 7.functions/immediately
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : Function with immediate execution
+ * Created on  : 2021-05-08 19:21:26.752331
+ * ===========================================================================
+ */
+
+(function foo() {
+    console.log("Foo was executed");
+})();
