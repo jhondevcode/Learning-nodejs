4735b8fb6a23979469a088adbd59c7da42bb4136
jhondev-code <jhondev.code@gmail.com>
2021-05-08 19:11:16 -0500

    A basic function in javascript

---
+++ 7.functions/basic/index.js
@@ -0,0 +1,20 @@
+/*
+ * ===========================================================================
+ * Name        : 7.functions/basic
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : A basic definition of a function
+ * Created on  : 2021-05-08 18:39:38.656421
+ * ===========================================================================
+ */
+
+function hello() {
+    console.log("Hello world!");
+}
+
+function bye() {
+    console.log("Bye bye");
+}
+
+hello();
+bye();
