ff7fb4e7923443ceb5b0c21d5f881fd5d3f2e9e5
jhondev-code <jhondev.code@gmail.com>
2021-05-08 19:19:19 -0500

    Return functions

---
+++ 7.functions/return/index.js
@@ -0,0 +1,32 @@
+/*
+ * ===========================================================================
+ * Name        : 7.functions/return
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : Functions with return
+ * Created on  : 2021-05-08 18:40:08.999557
+ * ===========================================================================
+ */
+
+// Simple math functions
+
+function addition(a, b) {
+    return a + b;
+}
+
+function substraction(a ,b) {
+    return a - b;
+}
+
+function multiplication(a , b) {
+    return a * b;
+}
+
+function division(a , b) {
+    return a / b;
+}
+
+console.log(addition(20, 30));
+console.log(substraction(285, 96));
+console.log(multiplication(32, 45));
+console.log(division(65, 3));
