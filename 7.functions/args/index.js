c62612ca7ed6666555afd5e685a5cbff80423a7a
jhondev-code <jhondev.code@gmail.com>
2021-05-08 19:15:12 -0500

    Parameter passing functions

---
+++ 7.functions/args/index.js
@@ -0,0 +1,26 @@
+/*
+ * ===========================================================================
+ * Name        : 7.functions/args
+ * Author      : Jhon
+ * Version     : 1.0.0
+ * Description : Functions with arguments
+ * Created on  : 2021-05-08 18:40:27.924843
+ * ===========================================================================
+ */
+
+function greets(name) {
+    console.log("Hello " + name);
+}
+
+function addition(a, b) {
+    console.log(a + b);
+}
+
+function substraction(a, b) {
+    console.log(a - b); 
+}
+
+greets("user");
+
+addition(10, 15);
+substraction(28, 17);
